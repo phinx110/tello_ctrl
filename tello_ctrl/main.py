173438f7a7e987e05ef5e8acc526e43882da22bc
Youri Vassiliev <youri@kapernikov.com>
2019-04-26 16:25:36 +0200

    Created a working python package with no functionality

---
+++ tello_ctrl/main.py
@@ -0,0 +1,83 @@
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+##from tello_msgs.msg import TelloAction
+from tello_msgs.srv import *
+
+
+
+class TelloScriptNode(Node):
+
+    def __init__(self):
+        super().__init__('TelloScriptNode')
+        self.publisher_ = self.create_publisher(String, 'topic')
+        self.cli = self.create_client(TelloAction, '/solo/tello_action')
+        
+        timer_period = 3  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        
+        
+        if(self.i % 2):
+            self.doTakeoff()
+        else:
+            self.doLand()
+            
+        self.i += 1
+    
+
+    def doTakeoff(self):
+        self.sendTelloCommand("takeoff")
+
+    def doLand(self):
+        self.sendTelloCommand("land")
+
+
+    def sendTelloCommand(self,cmd):
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+            
+        req = TelloAction.Request()
+        req.cmd = cmd
+        
+        future = self.cli.call_async(req)
+        rclpy.spin_until_future_complete(self, future)    
+        
+        if future.result() is not None:
+            self.get_logger().info(cmd + ' okay')
+        else:
+            self.get_logger().info(cmd + ' fail ' % (future.exception(),))
+
+
+        
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    sn  = TelloScriptNode()
+    
+    #sn.doTakeoff()
+    
+    #rclpy.spin_once(sn,timeout_sec=4)
+    
+    
+    #sn.doLand()
+    
+    
+    rclpy.spin(sn)
+    
+    
+    
+    sn.destroy_node()
+    rclpy.shutdown()
